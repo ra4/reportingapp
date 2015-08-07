ab69a550e6fb71dba1c6f29d5c5e3a8a1c909a49
Rajat Verma <rajatverma.me@gmail.com>
2015-08-07 19:29:46 +0530

    [Mail] Email fix

--- config/mail.php
+++ config/mail.php
@@ -54,7 +54,7 @@ return [
     |
     */
 
-    'from' => ['address' => null, 'name' => null],
+    'from' => ['address' => 'pilotdev.me@gmail.com', 'name' => 'Pilot Developer'],
 
     /*
     |--------------------------------------------------------------------------
