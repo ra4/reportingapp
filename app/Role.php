2b27ee12527f938444331c8076ec871d76fc65cc
Rajat Verma <rajatverma.me@gmail.com>
2015-08-11 12:03:21 +0530

    Revert "[ACL] Added the library and modified the code for ACL. Work in progress."  Shifted the code to a new branch named ACL

--- app/Role.php
+++ app/Role.php
@@ -4,12 +4,7 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
-class Role extends Model {
-
-    protected $table = 'roles';
-
-    public function users() {
-        return $this->hasMany('App\User', 'role_id', 'id');
-    }
-
+class Role extends Model
+{
+    //
 }
